486fdcff719ebe80b8117b17419de00cc9926d11
Lucie Ross <l.ross20@rgu.ac.uk>
2025-01-23 11:15:25 +0000

    my third server fix

--- lab_10/mythirdserver.js
+++ lab_10/mythirdserver.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var currentdate = require('knock-knock-jokes');
+var knockknock = require('knock-knock-jokes');
 
 http.createServer(function (req, res) {
  res.writeHead(200, {'Content-Type': 'text/html'});
