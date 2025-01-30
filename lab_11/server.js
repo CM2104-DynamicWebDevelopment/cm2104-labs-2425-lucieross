84e4da25960c86c31552b8ef02c42aa62006d6c4
Lucie Ross <l.ross20@rgu.ac.uk>
2025-01-30 12:33:14 +0000

    Form handing

--- lab_11/server.js
+++ lab_11/server.js
@@ -48,5 +48,11 @@ app.get('/calc', function(req, res) {
     res.send(result.toString());
 });
 
+app.get('/getform', function(req, res){
+    var name = req.query.name;
+    var quest = req.query.quest;
+     res.send("Hi "+name+" I am sure you will "+quest) ;
+});
+
 
 app.listen(8080);
