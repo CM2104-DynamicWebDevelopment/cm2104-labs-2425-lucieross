a338c52da7e20493160b11a3b0f88d02b18b249a
Lucie Ross <l.ross20@rgu.ac.uk>
2025-01-30 11:47:25 +0000

    Knockknock

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var knockknock = require('knock-knock-jokes');
 
 app.get('/', function(req, res){
     res.send("Hello world! by express");
@@ -8,5 +9,13 @@ app.get('/', function(req, res){
 app.get('/test', function(req, res){
     res.send("this is route 2");
 });
+
+app.get('/joke', function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var randomJoke = knockknock()
+    res.end(randomJoke)
+});
+
+
 
 app.listen(8080);
