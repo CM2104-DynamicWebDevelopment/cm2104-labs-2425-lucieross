68a21b5b14bce28424700f5869fc8e61a084ee09
Lucie Ross <l.ross20@rgu.ac.uk>
2025-01-30 12:30:02 +0000

    placement fix

--- lab_11/server.js
+++ lab_11/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var knockknock = require('knock-knock-jokes');
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
     res.send("Hello world! by express");
 });
@@ -48,5 +50,3 @@ app.get('/calc', function(req, res) {
 
 
 app.listen(8080);
-
-app.use(express.static('public'));
