344e4c999bd674347d59f7ee6fef8c078ea87676
Lucie Ross <l.ross20@rgu.ac.uk>
2025-02-05 23:29:46 +0000

    Top tracks

--- lab_12/server.js
+++ lab_12/server.js
@@ -13,18 +13,18 @@ var spotifyAPI = new SpotifyWebAPI({
     clientSecret: '75701a2b46fa4ffcaf93209a6223e7a5'
 })
 
-//Recieve an access token
+//rsecieve an access token
 spotifyAPI.clientCredentialsGrant().then(
     function (data) {
         console.log('The access token expires in ' + data.body['expires_in']);
         console.log('The acess token is ' + data.body['access_token']);
 
-        //Save token
+        //Saves token
         spotifyAPI.setAccessToken(data.body['access_token']);
     },
 
     function (err) {
-        console.log ('Something went wrong when recieveing access token',
+        console.log ('something went wrong when recieveing access token',
         err.message
         );
 
@@ -45,8 +45,9 @@ async function getTracks(searchterm, res) {
                 "<h2>" + track.name+"</h2>" +
                 "<h4>" + track.artists[0].name+"</h4>"+
                 "<img src = '"+ track.album.images[0].url+"'>"+
-                "<div>" + //added div for formatting better
+                "<div>" + //added div for formatting
                     "<a href='" + track.external_urls.spotify + "'>Track details</a>" +
+                    "<a href='/artistTopTracks/" + track.artists[0].id + "'>Get Top Tracks</a>" +
                 "</div>" +
             "</div>";
             console.log(HTMLResponce);
@@ -57,14 +58,28 @@ async function getTracks(searchterm, res) {
     });
 }
 
+async function getTopTracks(artist, res) { //gets artists top tracks
+    spotifyApi.getArtistTopTracks(artist,'GB')
+        .then(function (data) {
+            console.log(data.body);
+        }, function (err) {
+            console.log('Something went wrong!', err);
+    });
+}
+
 
 app.get('/searchLove', function (req,res){
     getTracks('love', res);
 });
 
-app.get('/search', function(req, res){
+app.get('/search', function(req, res){ //https://pantherdallas-tribunechris-8080.codio.io/form.html
     var searchterm = req.query.searchterm;
     getTracks(searchterm, res);
+})
+
+app.get('/artistTopTracks/:artistId', function (req, res){ //gets artist ID
+    var artistId = req.params.artistId;
+    getTopTracks(artistId,res); //shows top tracks
 })
 
 app.listen(8080);
