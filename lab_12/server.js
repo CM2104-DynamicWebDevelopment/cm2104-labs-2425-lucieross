301de70316b65b921751de5c104bd8b37932f84c
Lucie Ross <l.ross20@rgu.ac.uk>
2025-02-05 22:50:19 +0000

    SpotifyCode

--- lab_12/server.js
+++ lab_12/server.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var app = express();
-app.use(express.static('public'))
+var SpotifyWebAPI = require('spotify-web-api-node');
+app.use(express.static('public'));
 
-app.get('/'
-, function(req, res){
-    res.send("Hello world! by express");
+var spotifyAPI = new SpotifyWebAPI({
+    clientId: '7c9ea5ba3f3242408bdee87a90f83a0a',
+    clientSecret: '75701a2b46fa4ffcaf93209a6223e7a5'
+})
+
+//Recieve an access token
+spotifyAPI.clientCredentialsGrant().then(
+    function (data) {
+        console.log('The access token expires in ' + data.body['expires_in']);
+        console.log('The acess token is ' + data.body['access_token']);
+
+        //Save token
+        spotifyAPI.setAccessToken(data.body['access_token']);
+    },
+
+    function (err) {
+        console.log ('Something went wrong when recieveing access token',
+        err.message
+        );
+
+    }
+);
+
+async function getTracks(searchterm, res) {  
+    spotifyAPI.searchTracks(searchterm)
+    .then (function (data){
+        res.send(JSON.stringify(data.body));
+    }, function (err){
+        console.error(err);
+    });
+}
+
+app.get('/searchLove', function (req,res){
+    getTracks('love', res);
 });
-app.listen(8080);
