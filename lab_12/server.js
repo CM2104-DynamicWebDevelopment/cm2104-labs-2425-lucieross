67d538b9308839c5fecb81f84fd6f513d966a701
Lucie Ross <l.ross20@rgu.ac.uk>
2025-02-05 23:05:56 +0000

    fix

--- lab_12/server.js
+++ lab_12/server.js
@@ -40,7 +40,7 @@ async function getTracks(searchterm, res) {
                 "<h2>" + track.name+"</h2>" +
                 "<h4>" + track.artists[0].name+"</h4>"+
                 "<img src = '"+ track.album.images[0].url+"'>"+
-                "<a href='"+track.external_urls.spotify+"'> Track details </a>" +
+                "</n a href='"+track.external_urls.spotify+"'> Track details </a>" +
             "</div>";
             console.log(HTMLResponce);
         }
