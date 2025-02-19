c3b2677c324b0f561a7d11401ab2703f2cf16375
Lucie Ross <l.ross20@rgu.ac.uk>
2025-02-19 20:04:38 +0000

    Using values

--- lab_14/server.js
+++ lab_14/server.js
@@ -9,8 +9,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // index route
-app.get('/', (req, res) => {
-  res.render('pages/index');
+//app.get('/', (req, res) => {
+//  res.render('pages/index');
+//});
+
+app.get('/', function(req, res) {
+    var drinks = [
+    { name: 'Bloody Mary', drunkness: 3 },
+    { name: 'Martini', drunkness: 5 },
+    { name: 'Scotch', drunkness: 10 }
+    ];
+    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+    res.render('pages/index', {
+    drinks: drinks,
+    tagline: tagline
+    });
 });
 
 // about page
