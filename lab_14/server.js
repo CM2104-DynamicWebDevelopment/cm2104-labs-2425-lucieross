6714fcdeeef3fb0232b4a67ee583ad118b6e03b1
Lucie Ross <l.ross20@rgu.ac.uk>
2025-02-19 19:56:55 +0000

    server

--- lab_14/server.js
+++ lab_14/server.js
@@ -1,22 +1,16 @@
-// server.js
-// load the things we need
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
-// set the view engine to ejs
+// Set views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Set view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
-
-// index page
-app.get('/', function(req, res) {
-    res.render('pages/index');
-});
-
-// about page
-app.get('/about', function(req, res) {
-    res.render('pages/about');
+// Example route
+app.get('/', (req, res) => {
+  res.render('pages/index');
 });
 
 app.listen(8080);
-console.log('8080 is the magic port');
