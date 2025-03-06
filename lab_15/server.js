941c34d8ca871633593e78985967bc5820802f38
Lucie Ross <l.ross20@rgu.ac.uk>
2025-03-06 12:11:07 +0000

    console.log agin

--- lab_15/server.js
+++ lab_15/server.js
@@ -85,7 +85,7 @@ app.get('/profile', function(req, res) {
   
   
   var uname = req.query.username;
-  console.log(uname);
+  console.log(user.name.first);
   
  
   db.collection('people').findOne({"login.username": uname}, function(err, result) {
