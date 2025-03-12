6734681377f64bdea908ba9cf99811d48f554df4
Lucie Ross <l.ross20@rgu.ac.uk>
2025-03-12 22:41:13 +0000

    username

--- lab_16/public/scripts/main.js
+++ lab_16/public/scripts/main.js
@@ -3,14 +3,15 @@ var socket = io();
 //On form submit gets the context and emits a chat message
 $('#form').submit(function () {
     var message = $('#input').val();
-    if (message) {
-    socket.emit('chat message', message);
+    var username = $('#username').val(); 
+    if (message && username) {
+    socket.emit('chat message', username, message);
     $("#input").val("");
     }
     return false;
 })
 
 socket.on('chat message', function(msg) { //adds message to the message list
-    $('#messages').append("<li>"+msg+"</li>");
-    window.scrollTo(0, document.body.scrollHeight);
+    $('#messages').append("<li><strong>" + msg.username + ":</strong> " + msg.message + "</li>");
+    window.scrollTo(0, document.body.scrollHeight); //scrolls to the latest message
 });
--- lab_16/server.js
+++ lab_16/server.js
@@ -26,5 +26,5 @@ io.on('connection', function (socket) {
 });
 
 http.listen(8080, function(){
-    console.log('listening on *:8080');
+    console.log('listening on :8080');
 });
